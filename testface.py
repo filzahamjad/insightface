98e845a7e8f2f25ceeba62c288c927e96dbdef3b
Filzah Amjad <40711822+filzahamjad@users.noreply.github.com>
2026-01-21 15:26:30 +0500

    new test images

--- testface.py
+++ testface.py
@@ -19,4 +19,4 @@ def check_same_person(img1_path, img2_path):
         print(f"Error: {e}")
 
 # Usage
-check_same_person("face1/ID_2.jpg", "face1/Selfie_13.jpg")
+check_same_person("face3/ID_2.jpg", "face3/Selfie_1.jpg")
